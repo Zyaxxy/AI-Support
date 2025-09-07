138f3fe165c6dda7cdf08824fcb988a072cca7dd
Zyaxxy <utkarshjaiswal285@gmail.com>
2025-09-07 17:49:01 +0530

    feat: integrate Convex for user data fetching and update project structure

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,18 @@
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users  = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World Widget</h1>
-        <Button size="sm">Button</Button>
-        <Input placeholder="Input" />
+      <div className="flex flex-col items-center justify-center">
+        apps/web
+        <div className="max-w-sm w-full mx-auto gap-y-4">
+          {JSON.stringify(users, null, 2)}
+        </div>
+        
       </div>
     </div>
   )
