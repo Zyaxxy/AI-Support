4041d5e7d761eaa398a62afe92165c5013f619c1
Zyaxxy <utkarshjaiswal285@gmail.com>
2025-09-17 23:15:34 +0530

    sentry integration

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4e67a5d7414202cd4e3f3ba7372711d3@o4510036131053568.ingest.de.sentry.io/4510036142260304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
