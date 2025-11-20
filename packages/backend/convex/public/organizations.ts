0f2bf0d5c0b214d850dc4ade8388b6995eecff5a
Zyaxxy <utkarshjaiswal285@gmail.com>
2025-11-20 23:56:20 +0530

    Added Contact session Validation

---
+++ packages/backend/convex/public/organizations.ts
@@ -0,0 +1,24 @@
+import { v } from "convex/values";
+import { mutation } from "../_generated/server";
+import { createClerkClient } from "@clerk/backend";
+
+const clerkClient = createClerkClient({
+    secretKey: process.env.CLERK_SECRET_KEY,
+});
+
+export const validate = mutation({
+    args: {
+        organizationId: v.string(),
+    },  
+    handler: async (ctx, args ) => {
+       try {
+            await clerkClient.organizations.getOrganization({
+                organizationId: args.organizationId,
+            });
+            return {valid: true};
+       } catch (error) {
+            return {valid: false , reason: "organization not found"};
+       }
+    },
+});
+
