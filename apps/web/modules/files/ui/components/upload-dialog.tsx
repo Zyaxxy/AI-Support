3f28d596b4fb89a5c6b593f06c0ecb0d34676d85
Zyaxxy <utkarshjaiswal285@gmail.com>
2026-01-16 22:37:24 +0530

    Checkpoint Upload Dialog Box

--- apps/web/modules/files/ui/components/upload-dialog.tsx
+++ apps/web/modules/files/ui/components/upload-dialog.tsx
@@ -2,6 +2,7 @@
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -19,12 +20,50 @@ interface UploadDialogProps {
     onFileUploaded?: () => void;
 }
 export const UploadDialog = ({ open, onOpenChange, onFileUploaded }: UploadDialogProps) => {
+    const addFile = useAction(api.private.files.addFile);
+    const [ uploadedFile, setUploadedFiles ] = useState<File[]>([]);
+    const [ uploadForm , setUploadForms ] = useState({
+        category : "",
+        filename :"",
+        
+    });
+    const handleFileDrop = (files: File[]) => {
+        const file = files[0];
+        if(file) {
+            setUploadedFiles([file]);
+            if(!uploadForm.filename){
+                setUploadForms((prev) => ({
+                    ...prev,
+                    filename : file.name
+                }));
+            }
+        }
+    };
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent>
+            <DialogContent className="sm:max-w-lg">
                 <DialogHeader>
                     <DialogTitle>Upload File</DialogTitle>
+                    <DialogDescription>
+                        Upload a file to the knowledge base for AI Powered Search and Retrieval.
+                    </DialogDescription>
                 </DialogHeader>
+                <div className="space-y-2">
+                    <Label htmlFor="category">Category</Label>
+                    <Input
+                        className="w-full"  
+                        id="category"
+                        value={uploadForm.category}
+                        onChange={(e) => setUploadForms((prev) => ({ ...prev, category: e.target.value }))}
+                        placeholder="e.g. Documentation,Support,FAQs"
+                    />
+
+                </div>
+                <Dropzone onDrop={handleFileDrop}>
+                    <DropzoneContent>
+                        
+                    </DropzoneContent>
+                </Dropzone>
             </DialogContent>
         </Dialog>
     );
