7d2befec38904ca8c4a5dc8976f3e60659bcdebb
Zyaxxy <utkarshjaiswal285@gmail.com>
2026-02-05 22:45:01 +0530

    added get Vapi Phone Numbers Function

---
+++ packages/backend/convex/private/vapi.ts
@@ -0,0 +1,62 @@
+import {VapiClient, Vapi} from "@vapi-ai/server-sdk";
+import { internal } from "../_generated/api";
+import { action } from "../_generated/server";
+import { getSecretValue,parseSecretString } from "../lib/secrets";
+import { ConvexError } from "convex/values";
+
+export const getPhoneNumber = action({
+    args: {},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+            throw new ConvexError({
+                code: "UNAUTHORIZED",
+                message: "Identity not found"
+            })
+        }
+        const orgId = identity.orgId as string;
+                
+        if (!orgId) {
+            throw new ConvexError({
+                code: "UNAUTHORIZED",
+                message: "Organization not found"
+            })
+        }
+        
+        const plugin = await ctx.runQuery(internal.system.plugins.getByOrganizationIdAndService, {
+            organizationId: orgId,
+            service: "vapi",
+        });
+        if (!plugin) {
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Plugin not found"
+            })
+        }
+
+        const secretName = plugin.secretName;
+        const secret = await getSecretValue(secretName);
+        const secretData = parseSecretString<{
+            privateApiKey: string;
+            publicApiKey: string;
+        }>(secret);
+        if (!secretData) {
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Secret not found"
+            })
+        }
+        if(!secretData.privateApiKey || !secretData.publicApiKey) {
+            throw new ConvexError({
+                code: "NOT_FOUND",
+                message: "Credentials incomplete, Please update your Vapi credentials"
+            })
+        }
+        const vapiClient = new VapiClient({
+            token:secretData.privateApiKey,
+        }) 
+
+        const phoneNumber = await vapiClient.phoneNumbers.list();
+        return phoneNumber;
+    }
+});
